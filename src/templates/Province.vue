5eabf7c73a8c3b227add1e2f00769a7379e69112
Chris Rault <chris@smokeyfro.com>
2019-09-03 14:03:10 +0200

    Set province page to two columns

--- src/templates/Province.vue
+++ src/templates/Province.vue
@@ -3,23 +3,29 @@
     <div>
       <h1 class="title" v-html="$page.post.title" />
       <div v-html="$page.post.excerpt" class="text-gray-800 text-xl mb-5 mr-20" />
-      <div v-html="$page.post.content" class="markdown" />
-      <div v-if="$page.places.edges" class="mt-10">
-        <h2 class="text-3xl font-bold text-black">Towns / Cities</h2>
-        <div class="grid mt-5 mb-10">
-          <place-card-small class="" v-for="edge in $page.places.edges" :key="edge.node.id" :post="edge.node" />
+      <div class="flex flex-wrap">
+        <div class="w-1/2">
+          <div v-html="$page.post.content" class="markdown mr-20" />
         </div>
-      </div>
-      <div v-if="$page.hosts.edges" class="mt-10">
-        <h2 class="text-3xl font-bold text-black">Places to stay</h2>
-        <div class="grid-small mt-5">
-          <host-card-small class="" v-for="edge in $page.hosts.edges" :key="edge.node.id" :post="edge.node" />
-        </div>
-      </div>
-      <div v-if="$page.attractions.edges" class="mt-10">
-        <h2 class="text-3xl font-bold text-black">Things to see</h2>
-        <div class="grid-small mt-5">
-          <attraction-card-small class="" v-for="edge in $page.attractions.edges" :key="edge.node.id" :post="edge.node" />
+        <div class="w-1/2">
+          <div v-if="$page.places.edges" class="mt-10">
+            <h2 class="text-3xl font-bold text-black">Towns / Cities</h2>
+            <div class="grid mt-5 mb-10">
+              <place-card-small class="" v-for="edge in $page.places.edges" :key="edge.node.id" :post="edge.node" />
+            </div>
+          </div>
+          <div v-if="$page.hosts.edges" class="mt-10">
+            <h2 class="text-3xl font-bold text-black">Places to stay</h2>
+            <div class="grid-small mt-5">
+              <host-card-small class="" v-for="edge in $page.hosts.edges" :key="edge.node.id" :post="edge.node" />
+            </div>
+          </div>
+          <div v-if="$page.attractions.edges" class="mt-10">
+            <h2 class="text-3xl font-bold text-black">Things to see</h2>
+            <div class="grid-small mt-5">
+              <attraction-card-small class="" v-for="edge in $page.attractions.edges" :key="edge.node.id" :post="edge.node" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
