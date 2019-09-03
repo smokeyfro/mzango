a0a4cb581e530ce3fc4b78954dcb9f5ea273c630
Chris Rault <chris@smokeyfro.com>
2019-09-03 11:26:27 +0200

    Added the provincial map

--- src/pages/Provinces.vue
+++ src/pages/Provinces.vue
@@ -4,14 +4,19 @@
     <div class="">
       <div class="">
         <h1>Provinces</h1>
-        <p></p>
+        <p class="text-gray-800 text-xl">Below are the nine provinces that make up South Africa.</p>
       </div>
     </div>
     <div class="flex flex-wrap mt-10">
-      <div class="w-1/3 mb-5" v-for="item in $page.districts.edges" :key="item.node.id">
-        <h2 class="text-3xl sm:text-4xl leading-tight mb-1 sm:mb-2">
-          <g-link :to="`${item.node.path}`" class="text-black font-bold link">{{ item.node.title }}</g-link>
-        </h2>
+      <div class="districts w-1/3">
+        <div class="mb-5" v-for="item in $page.districts.edges" :key="item.node.id">
+          <h2 class="text-3xl sm:text-4xl leading-tight">
+            <g-link :to="`${item.node.path}`" class="text-black font-bold link">{{ item.node.title }}</g-link>
+          </h2>
+        </div>
+      </div>
+      <div class="map w-2/3 -mt-20">
+        <g-image src="../../media/site/south-africa-map.svg"></g-image>
       </div>
     </div>
 
