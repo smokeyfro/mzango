8408a9ce9a8dd5897eab6857234600d9044989c2
Chris Rault <chris@smokeyfro.com>
2019-09-03 11:44:14 +0200

    Conditional wrap on host & enabled postcss plugins

--- src/templates/Host.vue
+++ src/templates/Host.vue
@@ -6,16 +6,16 @@
         <div class="text-gray-900 text-xl -mt-3 mb-5"><span v-if="$page.post.province" v-html="$page.post.province" /> > <span v-if="$page.post.district" v-html="$page.post.district" /> > <span v-if="$page.post.place" v-html="$page.post.place" /></div>
       </div>
       <figure v-if="$page.post.image" class="rounded-lg overflow-hidden mb-10">
-        <g-image :src="$page.post.image"></g-image>
+        <g-image :src="$page.post.image" class="object-fill w-full"></g-image>
       </figure>
       <div class="container markdown">
         <div class="flex flex-wrap">
-          <div class="w-1/2">
+          <div class="w-1/2 about">
           <h2>About</h2>
             <div class="text-gray-800 text-xl mb-5 mr-20" v-if="$page.post.excerpt" v-html="$page.post.excerpt" />
             <div class="mr-20 text-lg" v-if="$page.post.content" v-html="$page.post.content" />
           </div>
-          <div class="w-1/2">
+          <div class="w-1/2 contact-info">
             <h2>Contact Info</h2>
             <div class="mt-3 mb-2 flex flex-wrap justify-left w-full">
               <span class="mr-6 mb-3 inline-block" v-if="$page.post.telephone"><strong class="block">Telephone </strong>{{$page.post.telephone}} </span>
@@ -23,32 +23,36 @@
               <span class="mr-6 mb-3 inline-block" v-if="$page.post.email"><strong class="block">Email </strong><a :href="mailtoLink" target="_blank" :title="mailtoTitle">Send Message</a></span>
               <div class="mr-6 mb-3 inline-block" v-if="$page.post.website"><strong class="block">Website </strong><a :href="$page.post.website" target="_blank" rel="nofollow noopener" class="button">View Website</a></div>
             </div>
-            <div v-if="$page.post.address">
+            <div v-if="$page.post.address" class="address mb-10">
               <strong class="block">Address </strong>
               {{ $page.post.address }}<br>{{ $page.post.place }}, {{ $page.post.district }}, {{ $page.post.province }}
             </div>
-          <h2>Accommodation</h2>
-          <ul class="list flex flex-wrap justify-left">
-            <li class="mr-6 mb-3 inline-block" v-for="item in $page.post.accommodation_types" v-bind:key="item">
-              <svg xmlns="http://www.w3.org/2000/svg" class="text-green-700 inline -mt-1 mr-1" width="14" height="14" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" clip-rule="evenodd" viewBox="0 0 18 18">
-                <g fill="none" stroke="#000" stroke-width="2">
-                  <path stroke-width="1.6" d="M16.8 8.069v.736c-.002 4.387-3.613 7.995-8 7.995-4.389 0-8-3.611-8-8 0-4.389 3.611-8 8-8 1.122 0 2.231.236 3.256.693"/>
-                  <path stroke-width="1.6" d="M16.8 2.405l-8 8.008-2.4-2.4"/>
-                </g>
-              </svg> {{item}}
-            </li>
-          </ul>
-          <h2>Facilities</h2>
-          <ul class="list flex flex-wrap justify-left">
-            <li class="mr-6 mb-3 inline-block" v-for="item in $page.post.facilities" v-bind:key="item">
-              <svg xmlns="http://www.w3.org/2000/svg" class="text-green-700 inline -mt-1 mr-1" width="14" height="14" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" clip-rule="evenodd" viewBox="0 0 18 18">
-                <g fill="none" stroke="#000" stroke-width="2">
-                  <path stroke-width="1.6" d="M16.8 8.069v.736c-.002 4.387-3.613 7.995-8 7.995-4.389 0-8-3.611-8-8 0-4.389 3.611-8 8-8 1.122 0 2.231.236 3.256.693"/>
-                  <path stroke-width="1.6" d="M16.8 2.405l-8 8.008-2.4-2.4"/>
-                </g>
-              </svg> {{item}}
-            </li>
-          </ul>
+            <div v-if="$page.post.accommodation_types">
+              <h2>Accommodation</h2>
+              <ul class="list flex flex-wrap justify-left">
+                <li class="mr-6 mb-3 inline-block" v-for="item in $page.post.accommodation_types" v-bind:key="item">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-700 inline -mt-1 mr-1" width="14" height="14" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" clip-rule="evenodd" viewBox="0 0 18 18">
+                    <g fill="none" stroke="#000" stroke-width="2">
+                      <path stroke-width="1.6" d="M16.8 8.069v.736c-.002 4.387-3.613 7.995-8 7.995-4.389 0-8-3.611-8-8 0-4.389 3.611-8 8-8 1.122 0 2.231.236 3.256.693"/>
+                      <path stroke-width="1.6" d="M16.8 2.405l-8 8.008-2.4-2.4"/>
+                    </g>
+                  </svg> {{item}}
+                </li>
+              </ul>
+            </div>
+            <div v-if="$page.post.facilities">
+              <h2>Facilities</h2>
+              <ul class="list flex flex-wrap justify-left">
+                <li class="mr-6 mb-3 inline-block" v-for="item in $page.post.facilities" v-bind:key="item">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-700 inline -mt-1 mr-1" width="14" height="14" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" clip-rule="evenodd" viewBox="0 0 18 18">
+                    <g fill="none" stroke="#000" stroke-width="2">
+                      <path stroke-width="1.6" d="M16.8 8.069v.736c-.002 4.387-3.613 7.995-8 7.995-4.389 0-8-3.611-8-8 0-4.389 3.611-8 8-8 1.122 0 2.231.236 3.256.693"/>
+                      <path stroke-width="1.6" d="M16.8 2.405l-8 8.008-2.4-2.4"/>
+                    </g>
+                  </svg> {{item}}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div class="hidden"><span v-if="$page.post.longitude" v-html="$page.post.longitude" /> / <span v-if="$page.post.latitude" v-html="$page.post.latitude" /></div> 
